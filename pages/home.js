dd11c467814e7c969f41a57c2046c47cd70dcb07
Ricky Stoll <rwstoll@iu.edu>
2023-04-26 01:20:04 -0400

    background

--- pages/home.js
+++ pages/home.js
@@ -1,19 +1,31 @@
 import { useEffect } from "react";
 import Navbar from "@/components/Navbar";
+import styles from "@/styles/Home.module.css";
+import backgroundImage from "images/image.png"; // import the image
+
+import Image from "next/image";
 
 const Home = () => {
-
     useEffect(() => {
         console.log("Component mounted!");
     }, []);
 
-    return(
-        <>
+    return (
+        <div className={styles["home-background"]}>
             <Navbar />
-            <h1>Home Page</h1>
-            Displaying home page
-        </>
+            <h1>Welcome to the Home Page</h1>
+            <p>Displaying home page</p>
+            <div style={{ position: "relative", width: "100%", height: "100%" }}>
+                <Image
+                    src={backgroundImage}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Background"
+                />
+            </div>
+        </div>
     );
-}
+};
 
 export default Home;
+
